70dd7305410110563d865d3a5ce871adcd3c23b6
GandhiW <gandhiwinata05@gmail.com>
2025-02-02 13:48:04 +0700

    temp dulu aja, msh blm selesai

---
+++ Upload_image/upload.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const fileElem = document.getElementById("fileElem");
+    const preview = document.getElementById("preview");
+
+    fileElem.addEventListener("change", function(e) {
+      const file = e.target.files[0];
+
+      console.log("File yang dipilih:", file);
+
+      if (file && file.type.startsWith("image/")) {
+        const reader = new FileReader();
+
+        reader.onloadend = function() {
+          preview.src = reader.result;
+          console.log("URL data gambar:", reader.result);
+        }
+
+        reader.readAsDataURL(file);
+      } else {
+        preview.src = "#";
+        console.log("File bukan gambar atau tidak valid.");
+      }
+    });
+  });
+
+// navbar
+fetch("../navbar.html") // Pastikan path menuju navbar benar
+  .then(response => {
+      if (!response.ok) {
+          throw new Error("Failed to load navbar");
+      }
+      return response.text();
+  })
+  .then(data => {
+      document.getElementById("navbar").innerHTML = data;
+  })
+  .catch(error => console.error("Error loading navbar:", error));
